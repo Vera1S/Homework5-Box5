d16ab6cbbde169b48f9067d3044c08d1df773195
Vera1S <128926912+Vera1S@users.noreply.github.com>
2023-04-13 11:26:23 -0400

    Add files via upload
     1 уровень сложности: 
    Создайте класс Box, в котором будут три поля: ширина, высота, 
    длина этой коробки. Создайте внутри этого класса конструктор,
     который принимает 3 значения и определяет их в вышеописанные 
    поля, помимо этого в конструкторе подсчитывается объём коробки 
    и печатается на экран.

---
+++ Box.java
@@ -0,0 +1,34 @@
+package Homework;
+
+
+public class Box {
+
+        // переменные экземпляра (поля)
+        public int width;
+        public int height;
+        public int length;
+
+    // конструктор
+    public Box (int width, int height, int length){
+
+        this.width = width;
+        this.height = height;
+        this.length = length;
+
+        System.out.println();
+        System.out.println(3);
+
+    System.out.println(width + " " + height + " " + length);
+
+    System.out.println();
+        int a = 2 * 3 * 3;
+        System.out.println(a);
+
+
+
+
+    }
+
+
+}
+
---
+++ Three.java
@@ -0,0 +1,14 @@
+import Homework.Box;
+
+import javax.swing.*;
+
+public class Three {
+    public static void main(String[] args) {
+        Box box1 = new Box(2, 3, 3);
+
+
+        System.out.println(box1.width + " " +box1.height + " " +box1.length);
+
+
+    }
+}
